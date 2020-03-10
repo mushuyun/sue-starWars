de6f9d55469ab1cb04d3c0d730691bd108f784ea
Shuyun Mu <mushuyun@yahoo.com>
2020-03-10 16:33:28 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
-const PORT = process.env.PORT || 5000;
+const path = require("path");
+const PORT = process.env.PORT || 3000;
 
 const app = express();
-
 // Sets up the express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -32,12 +32,13 @@ const characters = [
 ];
 
 app.get("/", (req, res) => {
-  res.send("Welcome to the Star Wars Page!");
-  res.sendFile(path.join(__dirname,"view.html"));
+  res.sendFile(path.join(__dirname, "view.html"));
 });
-app.get("/add", (req, res) =>{
+
+app.get("/add", (req, res) => {
   res.sendFile(path.join(__dirname, "add.html"));
-})
+});
+
 app.get("/api/characters/:character", (req, res) => {
   const character = req.params.character;
   console.log(character);
@@ -56,7 +57,7 @@ app.get("/api/characters/:character", (req, res) => {
 app.post("/api/characters", (req, res) => {
   const newCharacter = req.body;
 
-  console.log(newCharacter);
+  newCharacter.routeName = req.body.name.split(" ").join("").toLowerCase();
 
   characters.push(newCharacter);
 
@@ -66,103 +67,3 @@ app.post("/api/characters", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is listening port: ${PORT}`);
 });
-
-// const express = require("express");
-// const PORT = 4100;
-
-// const app = express();
-// app.use(express.urlencoded({extended: true}));
-// app.use(express.json());
-
-// // const yoda = {
-// //     name: "Yoda",
-// //     role: "Jedi",
-// //     age: 900,
-// //     forcePoints: 2000,
-// // }
-
-// // const dartmore = {
-// //     name: "Darth",
-// //     role: "Lord",
-// //     age: 200,
-// //     forcePoints: 1000
-// // }
-
-// // const roby = {
-// //     name: "Roby",
-// //     role: "Lord",
-// //     age: 600,
-// //     forcePoints: 1200
-// // }
-
-// app.get("/", (req, res)=>{
-//     res.send("welcome to the star wars page");
-// });
-
-
-// // app.get("/yoda", (req, res)=>{
-// //     res.json(yoda);
-// // });
-
-
-// // app.get("/dartmore", (req, res)=>{
-// //     res.json(dartmore);
-// // });
-
-// // app.get("/roby", (req, res)=>{
-// //     res.json(dartmore);
-// // });
-
-
-// const characters = [
-//    {
-//        routeName:"yoda",
-//         name: "Yoda",
-//         role: "Jedi",
-//         age: 900,
-//         forcePoints: 2000,
-//     },
-    
-//    {
-//        routeName: "dartmore",
-//         name: "Darth",
-//         role: "Lord",
-//         age: 200,
-//         forcePoints: 1000
-//     },
-    
-//     {
-//         routeName:"roby",
-//         name: "Roby",
-//         role: "Lord",
-//         age: 600,
-//         forcePoints: 1200
-//     }
-     
-// ]
-
-// app.get("/api/characters/:character", (req, res)=>{
-//     const character = req.params.character;
-//     console.log(character);
-//     for(let i = 0; i<characters.length; i++){
-//         if(character===characters[i].routeName){
-            
-//                 return res.json(characters[i]);
-//         }
-//     }
-//     return res.send("no character found!");
-//     //res.end();
-// });
-
-// app.post("/api/charcters", (req, res) =>{
-//     const newCharacter = req.body;
-//     console.log(newCharacter);
-//     console.push(newCharacter);
-//     res.json(newCharacter);
-// });
-
-// app.listen(PORT, () =>{
-
-//     console.log(`server is listening PORT ${PORT}`);
-// });
-
